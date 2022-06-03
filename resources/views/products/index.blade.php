531d2d8a7b193403c2c4d0b62a288cf989bfd7c8
Israel Rocha <53264974+irochaorbiscompliance@users.noreply.github.com>
2022-06-03 16:25:07 -0400

    Starting ajax

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-
-
+<br>
 <table>
     <tr>
+        <th>CMD</th>
         <th>ID</th>
         <th>Make</th>
         <th>Model</th>
         <th>Description</th>
+        <th>Parameters</th>
     </tr>  
     @foreach($products as $product)
         <tr>
+            <td><a href="/products/{{ $product->id }}">Edit</a></td>
             <td>{{ $product->id }}</td>
             <td>{{ $product->make }}</td>
             <td>{{ $product->model }}</td>
             <td>{{ $product->description }}</td>
+            <td id="param{{ $product->id }}" onclick="paramInsert()"></td>
         </tr>
     @endforeach
 </table>
+
+<script>
+    function paramInsert() {
+        document.getElementById("param{{ $product->id }}").innerHTML = "Hello World";
+    }
+</script>
 
 
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
+<br>
+<p> {{ $product->make }} </p>
+<p> {{ $product->model }}</p>
+<p> {{ $product->description }}</p>
 
-    <p> {{ $product->make }} </p>
-    <p> {{ $product->model }}</p>
-    <p> {{ $product->description }}</p>
+
+<br>
+
+<div class="card">
+    <div class="card-header">
+        Radio Frequency
+    </div>
+    <div class="card-body">
+        <p class="card-title">Frequency allocation</p>
+        <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" aria-label=".form-control-sm example">
+    </div>
+</div>
 
 @endsection
