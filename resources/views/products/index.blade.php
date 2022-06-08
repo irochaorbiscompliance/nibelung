5a85b48ef1c2eb8a0d63188cb2711d85d80a7237
Israel Rocha <53264974+irochaorbiscompliance@users.noreply.github.com>
2022-06-08 12:51:36 -0400

    switches

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,64 +18,31 @@
             <td>{{ $product->make }}</td>
             <td>{{ $product->model }}</td>
             <td>{{ $product->description }}</td>
-            <td
-                data-bs-toggle="popover"
-                title="Edit parameters"
-                data-bs-content=
-                    "
-                    Hello
-                    "
-                contenteditable="true"
-                id="param{{ $product->id }}"
-                onclick="selectParamCell(this.id)"></td>              
+            <td id="param{{ $product->id }}">
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="switchRF{{ $product->id }}" onclick="selectRFSwitch(this.id)"> <span>Radio Frequency</span>
+                 </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="safety{{ $product->id }}"> <span>Electrical Safety</span>
+                </div>
+            </td>              
         </tr>
     @endforeach
 </table>
 
 <script>
-    function selectParamCell(clicked_id) {
-        document.getElementById(clicked_id).innerHTML = clicked_id;
+    function selectRFSwitch(clicked_id) {
+
     }
 </script>
 <br>
-
-<h1></h1>
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-<script>
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, /*True in order to edit popover html*/{html: true}));
-</script>
-
 <!--
 <script>
-    function selectParamCell(clicked_id) {
-        document.getElementById(clicked_id).innerHTML = clicked_id;
-    }
-</script>
-
-
-<br>
-<br>
-<button id="1" onClick="reply_click(this.id)">B1</button>
-<button id="2" onClick="reply_click(this.id)">B2</button>
-<button id="3" onClick="reply_click(this.id)">B3</button>
-<p id="response"></p>
-    
-<script type="text/javascript">
-  function reply_click(clicked_id)
-  {
-      document.getElementById("response").innerHTML = clicked_id;
-  }
-</script>
-
-<script>
-    function paramInsert() {
-        document.getElementById("param{{ $product->id }}").innerHTML = "Hello World";
-    }
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, /*True in order to edit popover html*/{html: true}));
 </script>
 -->
 @endsection
